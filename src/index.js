a632bd6f4162767a02a80da3feb5d3b134c7518d
Mann <mann@example.com>
2020-12-28 02:24:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./components/Header";
-import TodoList from "./components/TodoList";
-import AddBar from "./components/AddBar";
-
-const App = () => {
-  const data = [
-    { id:1, text: "Do it!" },
-    { id:2, text: "Do it!" },
-    { id:3, text: "Just Do it!", important: true },
-    { id:4, text: "Do it!" },
-  ];
-  return (
-    <div className="App">
-      <Header />
-      <TodoList todos={data} />
-      <AddBar />
-    </div>
-  );
-};
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
