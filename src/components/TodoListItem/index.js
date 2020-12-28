5897ad5d49870b61fe7eef714a94336dcab77705
Mann <mann@example.com>
2020-12-28 04:10:44 +0300

    added strikethrough

--- src/components/TodoListItem/index.js
+++ src/components/TodoListItem/index.js
@@ -2,14 +2,32 @@ import React from "react";
 import s from "./style.module.css";
 
 export default class TodoListItem extends React.Component {
+  state = {
+    done: false,
+  };
+
+  onTextClick = () => {
+    console.log(this.props.text);
+    this.setState(({ done }) => {
+      return { done: !done };
+    });
+  };
+
   render() {
     const { important, text } = this.props;
+    const { done } = this.state;
 
-    const style = {
+    let style = {
       color: important ? "blue" : "black",
     };
+
+    if (done) {
+      console.log("done");
+      style.textDecorationLine = "line-through";
+    }
+
     return (
-      <span className={s.item} style={style}>
+      <span className={s.item} style={style} onClick={this.onTextClick}>
         {text}
       </span>
     );
